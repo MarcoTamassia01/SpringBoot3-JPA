d64316cf761085f69568fbbd04eae253f0039e6b
Marco Sallas <marco.a.s.tamassia@gmail.com>
2024-03-14 10:00:38 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/educandoweb/course/repositories/OrderitemRepository.java
@@ -0,0 +1,12 @@
+ package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.OrderItem;
+import com.educandoweb.course.entities.pk.OrderItemPK;
+
+public interface OrderitemRepository extends JpaRepository<OrderItem, OrderItemPK>{
+	
+	
+
+}
